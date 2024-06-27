ad23aa3566111e6f588f32256bf5e4fdd77b5199
daniel <ds@ctrlaltdel.de>
2024-06-27 22:07:54 +0200

    wip

--- external-secrets/manifests/prepare.sh
+++ external-secrets/manifests/prepare.sh
@@ -8,8 +8,8 @@ echo -n "redhat-password: "
 read -s REDHAT_PASSWORD
 echo
 
-#podman login -u devops -p $STRAUBCLOUD_PASSWORD --authfile=.dockerconfig.json registry.straubcloud.de
-#podman login -u danielstraub -p $REDHAT_PASSWORD --authfile=.dockerconfig.json registry.redhat.io
+podman login -u devops -p $STRAUBCLOUD_PASSWORD --authfile=.dockerconfig.json registry.straubcloud.de
+podman login -u danielstraub -p $REDHAT_PASSWORD --authfile=.dockerconfig.json registry.redhat.io
 
 [[ ! -d .ssh ]] && mkdir .ssh
 cp ~/.ssh/id_rsa .ssh/
